5315037e26bdee4c1810dd7662f8dcbaeb5e46b1
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-04-05 17:53:46 -0500

    challenge 7 passes

--- code-challenges/array-methods/challenges-05.test.js
+++ code-challenges/array-methods/challenges-05.test.js
@@ -157,7 +157,8 @@ removeLastCharacters('Gregor', 9) returns ''
 ------------------------------------------------------------------------------------------------ */
 
 const removeLastCharacters = (str, numberOfCharacters) => {
-  // Solution code here...
+  let x = str.length;
+  return str.slice(0, x - numberOfCharacters);
 };
 
 
