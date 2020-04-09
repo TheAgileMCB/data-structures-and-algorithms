23f1f97d9deae0613016526c812ae84d315c2215
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-04-08 20:29:10 -0500

    Challenge 4 passes

--- code-challenges/array.filter/challenges-08.test.js
+++ code-challenges/array.filter/challenges-08.test.js
@@ -82,7 +82,7 @@ const snorlaxData = {
 };
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  // Solution code here...
+  return arr.filter(stat => stat.baseStat > minBaseStat)
 };
 
 /* ------------------------------------------------------------------------------------------------
