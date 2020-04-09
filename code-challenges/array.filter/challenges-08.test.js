fd263af037cbb4e516235cbdb524b81bf829b286
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-04-08 20:23:18 -0500

    challenge 3 passes

--- code-challenges/array.filter/challenges-08.test.js
+++ code-challenges/array.filter/challenges-08.test.js
@@ -38,7 +38,8 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  return arr.filter(value => !
+  forbiddenValues.includes(value));
 };
 
 /* ------------------------------------------------------------------------------------------------
