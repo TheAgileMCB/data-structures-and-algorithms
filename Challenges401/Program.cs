c16ee4554646607c388b2a954fee30558566c39c
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-04 15:44:16 -0500

    commented code, changed semantic variables, added whiteboard image

--- Challenges401/Program.cs
+++ Challenges401/Program.cs
@@ -6,29 +6,27 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-           
+            // set test array   
+            int[] testArray = { 1, 2, 3, 4, 5 };
 
-            int[] initialArray = { 1, 2, 3, 4, 5 };
-
-           
-
-            Console.WriteLine(String.Join(", ", ReverseArray(initialArray)));
+            // call function and output results
+            Console.WriteLine(String.Join(", ", ReverseArray(testArray)));
         }
-
         
-        
-        static int[] ReverseArray(int[] array)
+        // new function which returns an array of integers
+        static int[] ReverseArray(int[] initialArray)
         {
+            // create new array and set it's length equal to length of initial array
+            int[] reversedArray = new int[initialArray.Length];
 
-            int[] reversedArray = new int[array.Length];
-
-            for (int i = 0; i < array.Length; i++)
+            // loop through initial array 
+            for (int i = 0; i < initialArray.Length; i++)
             {
-
-                reversedArray.SetValue(array[array.Length - 1 - i], i);
+                // set array elements into new array in reverse order
+                reversedArray.SetValue(initialArray[initialArray.Length - 1 - i], i);
             }
-            return reversedArray;
-        
+            // return reversed array for use in the Main
+            return reversedArray;        
         }
     }
 }
