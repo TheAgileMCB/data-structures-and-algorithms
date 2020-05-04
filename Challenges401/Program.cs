f0122b65399a0fccf6516fe2f05582ec722c98aa
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-04 15:21:25 -0500

    Challenge 1 outputs our test array.

--- Challenges401/Program.cs
+++ Challenges401/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace challenges
+namespace Challenges
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+
+            int[] initialArray = { 1, 2, 3, 4, 5 };
+
+           
+
+            Console.WriteLine(String.Join(", ", ReverseArray(initialArray)));
+        }
+
+        
+        
+        static int[] ReverseArray(int[] array)
+        {
+
+            int[] reversedArray = new int[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                reversedArray.SetValue(array[array.Length - 1 - i], i);
+            }
+            return reversedArray;
+        
         }
     }
 }
+
+
