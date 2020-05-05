cca28902a83e2d569bb4afaa221266f3dbf57f1f
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-05 15:56:56 -0500

    added unsaved changes. Whoops.

--- Challenges.Tests/ArrayShiftTests.cs
+++ Challenges.Tests/ArrayShiftTests.cs
@@ -9,7 +9,7 @@ namespace Challenges.Tests
         [InlineData(new[] { 7, 67, 1 }, 54, new[] { 7, 67, 54, 1 })]
         [InlineData(new[] { 7, 67, 1, 8 }, 54, new[] { 7, 67, 54, 1, 8 })]
 
-        public void Can_shift_odd_length_array(int[] input, int num, int[] expected)
+        public void Can_shift_odd_length_array(int[] input, num, int[] expected)
         {
             // Arrange
             // from input
