e48dab286f6f2e5a9a96f5ada335d88a12616c51
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-06 21:18:50 -0500

    code is written and tests pass

--- Challenges.Tests/BinarySearchTests.cs
+++ Challenges.Tests/BinarySearchTests.cs
@@ -1,10 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Xunit;
 
 namespace Challenges.Tests
 {
-    class Class1
+    public class BinarySearchTests
     {
+        [Theory]
+        [InlineData(1, new int[0], -1)]
+        [InlineData(54, new[] { 1, 3, 6, 54 }, 3)]
+        [InlineData(54, new[] { 7, 9, 10, 80 }, -1)]
+        [InlineData(8, new[] { 7, 8, 9, 14, 22, 45, 67, 90, 100, 111 }, 1)]
+
+        public void Can_binary_search(int key, int[] input, int expected)
+        {
+            // Arrange
+            // from data
+
+            // Act
+            int result = ArrayMethods.BinarySearch(key, input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
     }
 }
---
+++ Challenges401/ArrayMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    public class ArrayMethods
+    {
+        public static int BinarySearch(int key, int[] array)
+        {
+            int min = 0;
+            int max = array.Length - 1;
+
+            while (min <= max)
+            {
+                int midpoint = (min + max) / 2;
+            if (key == array[midpoint])
+            {
+                    return midpoint;
+                }
+                else if (key < array[midpoint])
+                {
+                    max = midpoint - 1;
+                }
+                else
+                {
+                    min = midpoint + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
