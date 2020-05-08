fd309f62ef65fc00fb383875ebe8ccada298931a
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-08 18:51:35 -0500

    began link list project

---
+++ Challenges401/DataStructure/DataStructure/LinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class LinkedList
+    {
+        private Node head;
+
+        public void Insert(int value)
+        {
+            this.head = new Node(value);
+        }
+
+        public class Node
+        {
+            public Node(int value)
+            {
+                this.Value = value;
+                this.Next = null;
+            }
+
+            public int Value { get; set; }
+            public Node Next { get; set; }
+        }
+    }
+}
