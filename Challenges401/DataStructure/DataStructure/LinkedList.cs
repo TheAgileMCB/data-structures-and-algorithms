fb82cbe6c15f51eb4bf82478667cc97a462c803b
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-09 23:02:31 -0500

    added comments

--- Challenges401/DataStructure/DataStructure/LinkedList.cs
+++ Challenges401/DataStructure/DataStructure/LinkedList.cs
@@ -4,6 +4,7 @@ namespace DataStructures
 {
     public class LinkedList
     {
+        // establishes to head and current nodes for traversing to list
         private Node head;
         private Node current;
 
@@ -11,13 +12,17 @@ namespace DataStructures
         {
             try
             {
+                // assigns the new node to a temporary holding node
                 Node tempNode = new Node(value);
 
+                // checks that the list head exists
                 if (head != null)
                 {
+                    // if it does, sets the temp node to point at the current head
                     tempNode.Next = head;
                 }
 
+                // sets the value of the temp node to the head
                 head = tempNode;
                 current = tempNode;
             }
@@ -35,13 +40,19 @@ namespace DataStructures
         {
             try
             {
+                // assigns the head to the current ndoe
                 current = head;
+                //traverses the nodes, verifying that the nodes have content and that they are not the desired value
                 while ((current != null) && (current.Value != value))
                 {
+                    // assigns the next value to the current
                     current = current.Next;
                 }
+
+                // if the loop returns a value, it is the desired value so it returns true
                 if (current != null) return true;
 
+                //otherwise, it returns false
                 else return false;
             }
             catch (Exception ex)
@@ -57,17 +68,22 @@ namespace DataStructures
         {
             try
             {
+                // establish an empty string
                 string linkedListOutput = "";
 
+                // perform the actions if the nead node has data
                 if (head != null)
                 {
+                    // add the current value of the node the string until the next node returns null
                     linkedListOutput += $"{{{current.Value}}}";
                     while (current.Next != null)
                     {
+                        // adds the subsequent values to the string
                         current = current.Next;
                         linkedListOutput += $" -> {{{current.Value}}}";
                     }
                 }
+                // returns the string and concatenates the final null value
                 return linkedListOutput + " -> NULL";
             }
             catch (Exception ex)
@@ -78,7 +94,9 @@ namespace DataStructures
             }
 
         }
-
+        /// <summary>
+        /// Establishes the node class, getting and setting both an int value and the Node next
+        /// </summary>
         public class Node
         {
             public Node(int value)
