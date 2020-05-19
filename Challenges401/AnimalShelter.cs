7a95e7ed15b4b33b10b506a6f447b2d0235526e3
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-19 18:15:43 -0500

    scaffolded AnimalShelter and Enqueue function

--- Challenges401/AnimalShelter.cs
+++ Challenges401/AnimalShelter.cs
@@ -1,10 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Threading;
 
 namespace Challenges
 {
-    public class AnimalShelter
+    public class Shelter
     {
+        public Queue Dogs { get; set; }
+        public Queue Cats { get; set; }
+        //public Queue Dogs { get; set; }
+        public Shelter()
+        {
+            Dogs = new Queue();
+            Cats = new Queue();
+            
+        }
+
+    }
+
+    public class Queue
+    {
+        Node head = null;
+        Node tail = null;
+        int count = 0;
+
+        public Node Head
+        {
+            get
+            {
+                return head;
+            }
+        }
+
+        public Node Tail
+        {
+            get
+            {
+                return tail;
+            }
+        }
+
+        public void Enqueue(Node newNode)
+        {
+            if (count == 0)
+            {
+                head = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+            }
+
+            tail = newNode;
+            count++;
+        }
+    }
+    
+
+    public enum Animal
+    {
+        Dog = 1,
+        Cat = 2
+    }
+
+    
+
+    public class Node
+    {
+        public Animal Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(Animal value)
+        {
+            this.Value = value;
+        }
+
     }
 }
