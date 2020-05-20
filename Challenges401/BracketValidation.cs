0aad441b241207bb19d844f7b4fc3ccde56b4a13
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-20 15:29:37 -0500

    wrote function, but missing returns somewhere

--- Challenges401/BracketValidation.cs
+++ Challenges401/BracketValidation.cs
@@ -1,10 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+//using DataStructures;
 
 namespace Challenges
 {
     class BracketValidation
     {
+        public static bool IsBalanced(string input)
+        {
+            Dictionary<char, char> bracketPairs = new Dictionary<char, char>()
+            {
+                { '{', '}'},
+                { '(', ')'},
+                { '<', '>'},
+                { '[', ']'},
+             };
+
+            Stack<char> brackets = new Stack<char>();
+
+            try
+            {
+                foreach (char item in input)
+                {
+                    if (bracketPairs.Keys.Contains(item))
+                    {
+                        brackets.Push(item);
+
+                        if (bracketPairs.Values.Contains(item))
+                        {
+                            if (item == brackets.First())
+                            {
+                                brackets.Pop();
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            return false;
+                        }
+
+                        continue;
+                    }
+
+                }
+            }
+            catch 
+            {
+
+                return false;
+            }
+
+            if (brackets.Count == 0)
+            {
+                return true;
+            }
+        }
+
     }
 }
