0160ea99b718aca5e14a38af3596ee8ec7f34c16
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-20 13:57:46 -0500

    populated with stack tests from my weekend Data Structure

--- Challenges.Tests/BracketValidationTests.cs
+++ Challenges.Tests/BracketValidationTests.cs
@@ -1,10 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Text;
+using Xunit;
+using DataStructures;
 
 namespace Challenges.Tests
 {
-    class BracketValidationTests
+    public class BracketValidationTests
     {
+        [Fact]
+        public void Peek_empty_stack_throws()
+        {
+            // Arrange
+            Stack<char> bracket = new Stack<char>();
+
+            // Assert
+            Assert.Throws<StackEmptyException>(() =>
+            {
+                // Act
+                bracket.Peek();
+            });
+        }
+
+        [Fact(Skip = "pulled from StackTests")]
+        public void I_can_has_push()
+        {
+            // Arrange
+            Stack<char> brackets = new Stack<char>();
+            char expected = };
+
+        // Act
+        stack.Push(});
+
+
+            //Assert
+            Assert.Equal(expected, stack.Peek());
+
+        }
+
+        [Fact(Skip = "pulled from StackTests")]
+        public void I_can_has_many_push()
+        {
+            // Arrange
+            Stack<char> newStack = new Stack<char>();
+
+
+            // Act
+            newStack.Push(});
+            newStack.Push());
+            newStack.Push(>);
+            newStack.Push(>);
+
+            // Assert
+            Assert.Equal(4, newStack.count);
+            Assert.Equal(100, newStack.Peek());
+
+        }
     }
 }
