3250164eb8b919712a9cae26004c86f3f94e8b12
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-24 11:44:38 -0500

    built BST file and moved Insert method from BinaryTree file to it

---
+++ DataStructures/Binary Trees/BinarySearchTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Binary_Trees
+{
+    class BinarySearchTree<T> where
+        T : IComparable<T>
+    {
+        public Node Root;
+        public BinarySearchTree()
+        {
+            Root = null;
+        }
+
+        public Node ReturnRoot()
+        {
+            return Root;
+        }
+        public void Add(T nodeToBeAdded)
+        {
+            Node newNode = new Node();
+            newNode.Value = nodeToBeAdded;
+            if (Root == null)
+            {
+                Root = newNode;
+            }
+            else
+            {
+                Node current = Root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (nodeToBeAdded.CompareTo < current.Value)
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        current = current.Right;
+                        if (current == null)
+                        {
+                            parent.Right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
