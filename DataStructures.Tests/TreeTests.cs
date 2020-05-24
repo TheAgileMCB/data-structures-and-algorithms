06e98a16410b9edf1c06d971d2628339e5e35432
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-24 13:24:24 -0500

    skipped one test, add test fails

--- DataStructures.Tests/TreeTests.cs
+++ DataStructures.Tests/TreeTests.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using DataStructures.BinaryTrees;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace DataStructures.Tests
 {
-    class TreeTests
+    public class TreeTests
     {
+        [Fact (Skip = "how to assert a tree is empty?")]
+        public void Can_make_empty()
+        {
+            // Arrange
+            BinarySearchTree<int> testTree = new BinarySearchTree<int>();
 
+            // Act
+
+
+            // Assert
+            
+
+        }
+
+        [Fact]
+        public void Can_add_one_node()
+        {
+            // Arrange
+            BinarySearchTree<int> testTree = new BinarySearchTree<int>();
+            int expected = 7;
+
+
+            // Act
+            testTree.Add(5);
+
+            // Assert
+            Assert.Equal(expected, testTree.Root.Value);
+
+        }
     }
 }
