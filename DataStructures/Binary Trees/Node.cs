a0f81920aed061bd51134e9df6f52bd3645da8d7
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-24 11:46:21 -0500

    made external Node class, skeptical of its value, does not like generic T

---
+++ DataStructures/Binary Trees/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Binary_Trees
+{
+
+    public class Node
+    {
+        public T Value { get; set; }
+        public Node Root { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+
+}
