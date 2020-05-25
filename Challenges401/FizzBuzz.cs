31c6e6870e9af69d531d82131e0e245f25c844fd
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-25 17:55:07 -0500

    scaffolded FizzBuzz class

---
+++ Challenges401/FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿using DataStructures.BinaryTrees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    class FizzBuzz
+    {
+        public BinarySearchTree<string> FizzBuzzTree(BinarySearchTree<int> tree)
+        {
+
+        }
+    }
+}
