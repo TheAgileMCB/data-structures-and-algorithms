480939baa2d63a4fd4a5214e9c2be32984a7a14e
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-25 20:41:46 -0500

    test for fizz passes, wrote test for buzz

--- Challenges.Tests/FizzBuzzTests.cs
+++ Challenges.Tests/FizzBuzzTests.cs
@@ -53,5 +53,28 @@ namespace Challenges.Tests
             // Assert
             Assert.Equal(expected, actual.Root.Left.Value);
         }
+
+        [Fact]
+        public void Can_get_buzz()
+        {
+            // Arrange
+            BinarySearchTree<int> actualTree = new BinarySearchTree<int>();
+            actualTree.Add(15);
+            actualTree.Add(3);
+            actualTree.Add(30);
+            actualTree.Add(45);
+            actualTree.Add(5);
+            actualTree.Add(9);
+            actualTree.Add(10);
+            actualTree.Add(2);
+
+            string expected = "FizzBuzz";
+
+            // Act
+            var actual = FizzBuzz.FizzBuzzTree(actualTree);
+
+            // Assert
+            Assert.Equal(expected, actual.Root.Left.Right.Value);
+        }
     }
 }
