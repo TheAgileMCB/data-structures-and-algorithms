6d9842505a3aaa56f97ac04ada7c45a44036963e
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-25 19:55:04 -0500

    made IEnumerable group, attempting to iterate and modfiy

--- Challenges401/FizzBuzz.cs
+++ Challenges401/FizzBuzz.cs
@@ -1,15 +1,35 @@
-﻿using DataStructures.BinaryTrees;
+﻿
+using DataStructures.BinaryTrees;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Challenges
 {
-    class FizzBuzz
+    public class FizzBuzz 
     {
         public BinarySearchTree<string> FizzBuzzTree(BinarySearchTree<int> tree)
         {
+            int[] treeEnumerable = (int[])tree.PreOrder(tree.ReturnRoot());
 
+            foreach (var item in treeEnumerable)
+            {
+                if (item % 15 == 0)
+                {
+                    treeEnumerable[item].ToString();
+                    treeEnumerable[item] = "FizzBuzz";
+                }
+                else if (item % 5 == 0)
+                {
+                    item.ToString();
+                    item = "Buzz";
+                }
+                else if (item % 3 == 0)
+                    return "Fizz";
+                else item.ToString();
+            }
+                
         }
     }
 }
