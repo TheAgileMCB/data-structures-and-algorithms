b4a61920ba89df948f0a015e145c027cf2cf275a
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-27 15:27:13 -0500

    refactored Tree class

--- DataStructures/BinaryTrees/Tree.cs
+++ DataStructures/BinaryTrees/Tree.cs
@@ -95,7 +95,6 @@ namespace DataStructures.BinaryTrees
         public class Node
         {
             public T Value { get; set; }
-            public Node Root { get; set; }
             public Node Left { get; set; }
             public Node Right { get; set; }
         }
