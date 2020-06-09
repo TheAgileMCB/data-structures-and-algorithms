8667d8b34387c96198adadf450a6281a82430342
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-09 18:54:27 -0500

    created MergeSort file

---
+++ Challenges401/MSort.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges
+{
+    class MSort
+    {
+        public static int[] MergeSort(int[] inputArray)
+        {
+
+        }
+    }
+}
