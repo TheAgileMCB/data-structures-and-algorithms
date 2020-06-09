7cf867f477439a5b943e97391938f11f6869b5b8
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-09 08:20:11 -0500

    added Theory for empty array

--- Challenges.Tests/InsertSortTests.cs
+++ Challenges.Tests/InsertSortTests.cs
@@ -12,6 +12,7 @@ namespace Challenges.Tests
         [InlineData(new int[] { 20, 18, 12, 8, 5, -2 }, new int[] { -2, 5, 8, 12, 18, 20 })]
         [InlineData(new int[] { 5, 12, 7, 5, 5, 7 }, new int[] { 5, 5, 5, 7, 7, 12 })]
         [InlineData(new int[] { 2, 3, 5, 7, 13, 11 }, new int[] { 2, 3, 5, 7, 11, 13 })]
+        [InlineData(new int[] { }, new int[] { })]
         public void Can_sort(int[] input, int[] expected)
         {
             // Arrange (Theory)
