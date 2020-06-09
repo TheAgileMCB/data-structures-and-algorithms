741ea50700acf4be98d429b6cea775ea072219ca
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-09 08:17:10 -0500

    wrote logic for InsertionSort

--- Challenges401/InsertSort.cs
+++ Challenges401/InsertSort.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace Challenges
 {
-    class InsertSort
+    public class InsertSort
     {
+        public static int[] InsertionSort(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+            return inputArray;
+        }
     }
 }
