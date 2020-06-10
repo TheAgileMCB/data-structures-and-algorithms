da022248e3a57960a88e54c6ae6f587614e655c2
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-09 19:13:16 -0500

    wrote MergeSort logic

--- Challenges401/MSort.cs
+++ Challenges401/MSort.cs
@@ -6,9 +6,31 @@ namespace Challenges
 {
     public class MSort
     {
-        public static int[] MergeSort(int[] inputArray)
+        public static List<int> MergeSort(List<int> input)
         {
+            if (input.Count <= 1)
+                return input;
 
+            int midpoint = input.Count / 2;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            for(int i = 0; i <midpoint; i++)
+            {
+                left.Add(input[i]);
+            }
+            for (int i =midpoint; i < input.Count; i++)
+            {
+                right.Add(input[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+
+            return Merge(left, right);
         }
+
+        
     }
 }
