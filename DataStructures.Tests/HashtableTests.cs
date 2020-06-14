908aef1f643e47808d93955a437d0cc385071aab
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-14 15:21:58 -0500

    wrote and element test for hashtables

--- DataStructures.Tests/HashtableTests.cs
+++ DataStructures.Tests/HashtableTests.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using DataStructures.Hashtables;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace DataStructures.Tests
 {
     public class HashtableTests
     {
+        [Fact]
+        public void Can_has_element()
+        {
+            // Arrange
+            Hashtable testTable = new Hashtable();
+
+            // Act
+            testTable.Add("noodles", "spaghetti")
+
+        // Assert
+            Assert.True(testTable.Contains("noodles"));
+        }
     }
 }
