ea6545badf90286ca1f946f5c9ebc5098680d361
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-15 08:54:55 -0500

    wrote test for Contains for lower-level elements. Failed, now passes

--- DataStructures.Tests/HashtableTests.cs
+++ DataStructures.Tests/HashtableTests.cs
@@ -52,5 +52,23 @@ namespace DataStructures.Tests
             // Assert
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void Can_contains_lower_level_elementz()
+        {
+            // Arrange
+            Hashtable testTable = new Hashtable(1);
+
+            // Act
+            testTable.Add("noodles", "spaghetti");
+            testTable.Add("bread", "baguette");
+            testTable.Add("cake", "a lie");
+            testTable.Add("cheese", "swiss");
+            testTable.Add("casserole", "tater tot");
+
+
+            // Assert
+            Assert.True(testTable.Contains("cheese"));
+        }
     }
 }
