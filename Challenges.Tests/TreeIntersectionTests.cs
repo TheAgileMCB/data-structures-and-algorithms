0d9cacd62bb06cbbe907e513cd63fddc1e1d7070
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-16 14:08:56 -0500

    built a test and some theories

--- Challenges.Tests/TreeIntersectionTests.cs
+++ Challenges.Tests/TreeIntersectionTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructures.BinaryTrees;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -8,9 +9,32 @@ namespace Challenges.Tests
     public class TreeIntersectionTests
     {
         [Theory]
-        public void Can_return_list()
-        {
+        [InlineData(new int[] { 1, 5, 7, 8, 9, 99, 100, 150 }, new int[] { 1, 8, 90, 54, 657, 12, 9, 5 }, new string[] { "1", "5", "8", "9" })]
+        [InlineData(new int[] { 200, 2000, 5, 555, 789 }, new int[] { 2000, 555, 888, 123,3222 }, new string[] { "2000", "555" })]
+        [InlineData(new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 2, 2, 1, 1, 1, 1 }, new string[] { "1", "1", "1", "1", "1" })]
 
+
+        public void Can_return_list(int[] arrOne, int[] arrTwo, string[] expected)
+        {
+            // Arrange
+            BinarySearchTree<int> treeOne = new BinarySearchTree<int> { };
+            BinarySearchTree<int> treeTwo = new BinarySearchTree<int> { };
+
+            for(int i = 0; i <arrOne.Length; i++)
+            {
+                treeOne.Add(arrOne[i]);
+            }
+
+            for (int i = 0; i < arrTwo.Length; i++)
+            {
+                treeTwo.Add(arrOne[i]);
+            }
+
+            //Act
+            string[] actual = Hashtables.TreeIntersection.Tree_Intersection(treeOne, treeTwo);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
