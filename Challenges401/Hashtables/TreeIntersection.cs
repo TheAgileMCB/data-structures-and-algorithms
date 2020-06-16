49f418e8581a8c9d46e531365a5e9cb1837a0e31
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-16 15:09:55 -0500

    wrote Tree_Intersection logic

--- Challenges401/Hashtables/TreeIntersection.cs
+++ Challenges401/Hashtables/TreeIntersection.cs
@@ -1,15 +1,34 @@
 ﻿using DataStructures.BinaryTrees;
+using DataStructures.Hashtables;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Challenges.Hashtables
 {
-    class TreeIntersection
+    public class TreeIntersection
     {
-        public List<int> Tree_Intersection(DataStructures.BinaryTrees.BinarySearchTree<int> treeOne, DataStructures.BinaryTrees.BinarySearchTree<int> treeTwo)
+        public static List<string> Tree_Intersection(DataStructures.BinaryTrees.BinarySearchTree<int> treeOne, DataStructures.BinaryTrees.BinarySearchTree<int> treeTwo)
         {
+            Hashtable myTable = new Hashtable(7);
+            List<string> returnList = new List<string>();
 
+            foreach (int value in treeOne.PreOrder(treeOne.ReturnRoot()))
+            {
+                string keyOne = value.ToString();
+                myTable.Add(keyOne, null);
+            }
+            foreach (int value in treeTwo.PreOrder(treeTwo.ReturnRoot()))
+            {
+                string keyTwo = value.ToString();
+                if (myTable.Contains(keyTwo))
+                {
+                    returnList.Add(keyTwo);
+                }
+            }
+
+            return returnList;
         }
     }
 }
