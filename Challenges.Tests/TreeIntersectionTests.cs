1ff8026e07b98ed1706564979c8efc2d571df304
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-16 14:10:49 -0500

    added empty arrays theory

--- Challenges.Tests/TreeIntersectionTests.cs
+++ Challenges.Tests/TreeIntersectionTests.cs
@@ -12,7 +12,7 @@ namespace Challenges.Tests
         [InlineData(new int[] { 1, 5, 7, 8, 9, 99, 100, 150 }, new int[] { 1, 8, 90, 54, 657, 12, 9, 5 }, new string[] { "1", "5", "8", "9" })]
         [InlineData(new int[] { 200, 2000, 5, 555, 789 }, new int[] { 2000, 555, 888, 123,3222 }, new string[] { "2000", "555" })]
         [InlineData(new int[] { 1, 1, 1, 1, 1 }, new int[] { 1, 2, 2, 1, 1, 1, 1 }, new string[] { "1", "1", "1", "1", "1" })]
-
+        [InlineData(new int[] { }, new int[] { }, new string[] { })]
 
         public void Can_return_list(int[] arrOne, int[] arrTwo, string[] expected)
         {
