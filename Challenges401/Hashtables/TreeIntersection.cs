78f4f045420d834d2b42412c0ea5f731b94c62c2
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-16 13:26:46 -0500

    created TreeIntersection file

---
+++ Challenges401/Hashtables/TreeIntersection.cs
@@ -0,0 +1,15 @@
+﻿using DataStructures.BinaryTrees;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.Hashtables
+{
+    class TreeIntersection
+    {
+        public List<int> Tree_Intersection(DataStructures.BinaryTrees.BinarySearchTree<int> treeOne, DataStructures.BinaryTrees.BinarySearchTree<int> treeTwo)
+        {
+
+        }
+    }
+}
