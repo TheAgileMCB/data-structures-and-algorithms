d043f72d8683a8df85dc5440735570ca839d17b6
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-16 13:26:24 -0500

    began test file for Tree Intersections

---
+++ Challenges.Tests/TreeIntersectionTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Challenges.Tests
+{
+    public class TreeIntersectionTests
+    {
+        [Theory]
+        public void Can_return_list()
+        {
+
+        }
+    }
+}
