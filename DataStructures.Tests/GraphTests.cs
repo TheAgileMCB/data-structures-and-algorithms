9a973661e5740dd69b1c73fd7f40369109bfbe72
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-21 21:52:48 -0500

    wrote test for get all nodes

--- DataStructures.Tests/GraphTests.cs
+++ DataStructures.Tests/GraphTests.cs
@@ -9,16 +9,33 @@ namespace DataStructures.Tests
     public class GraphTests
     {
         [Fact]
-        public void I_can_add_a_node()
+        public void Can_add_a_node()
         {
-            //Arrange
+            // Arrange
             Graph<int> testGraph = new Graph<int>();
             
-            //Act
-            newGraph.AddNode(7);
+            // Act
+            testGraph.AddNode(7);
 
-            //Assert
-            Assert.Contains(7, testGraph);
+            // Assert
+            Assert.Contains(7, testGraph.GetNodes());
+        }
+
+        [Fact]
+        public void Can_get_all_nodes()
+        {
+            // Arrange
+            List<int> expected = new List<int> { 1, 1, 4, 7, 3, 2, 9, 122, 45, 56 };
+            Graph<int> testGraph = new Graph<int>();
+
+            // Act
+            foreach (int item in expected)
+            {
+                testGraph.AddNode(item);
+            }
+
+            // Assert
+            Assert.Equal(expected, testGraph.GetNodes());
         }
     }
 }
