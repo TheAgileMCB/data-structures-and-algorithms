e94423533d723cf026ec06e5c622bff69a88f9af
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-06-21 21:30:54 -0500

    wrote add node graph test

---
+++ DataStructures.Tests/GraphTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace DataStructures.Tests
+{
+    public class GraphTests
+    {
+        [Fact]
+        public void I_can_add_a_node()
+        {
+            //Arrange
+            Graph<int> testGraph = new Graph<int>();
+            
+            //Act
+            newGraph.AddNode(7);
+
+            //Assert
+            Assert.Contains(7, testGraph);
+        }
+    }
+}
